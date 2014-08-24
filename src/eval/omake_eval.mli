8cc320c6ca5317e4d0857e7283bc4ebe907ca86c
Hongbo Zhang <bobzhang1988@gmail.com>
2014-08-24 08:47:29 -0400

    in the middle of refactoring omake_eval

--- src/eval/omake_eval.mli
+++ src/eval/omake_eval.mli
@@ -1,45 +1,6 @@
 (*
  * Compile (evaluate) an OMakefile.
- *
- * ----------------------------------------------------------------
- *
- * @begin[license]
- * Copyright (C) 2003-2010 Mojave Group, California Institute of Technology, and
- * HRL Laboratories, LLC
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
- * of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- * Additional permission is given to link this library with the
- * with the Objective Caml runtime, and to redistribute the
- * linked executables.  See the file LICENSE.OMake for more details.
- *
- * Author: Jason Hickey @email{jyh@cs.caltech.edu}
- * Modified by: Aleksey Nogin @email{nogin@metaprl.org}, @email{anogin@hrl.com}
- * @end[license]
  *)
-
-open Lm_location
-
-open Omake_ir
-open Omake_env
-
-open Omake_node
-open Omake_ir_ast
-
-open! Omake_value_type
-open Omake_command_type
 
 val print_ast   : bool ref
 val print_ir    : bool ref
@@ -47,119 +8,118 @@ val print_rules : bool ref
 val print_files : bool ref
 val debug_eval  : bool ref
 
-(* Utility *)
-val raise_uncaught_exception : pos -> exn -> 'a
+val raise_uncaught_exception : Omake_value_type.pos -> exn -> 'a
 
 (*
  * Evaluate an expression.
  *)
-val eval : venv -> exp -> value
-val eval_exp : venv -> value -> exp -> venv * value
-val eval_sequence : venv -> pos -> value -> exp list -> venv * value
-val eval_sequence_export : venv -> pos -> value -> exp list -> export -> venv * value
-val eval_sequence_exp : venv -> pos -> exp list -> venv * value
-val eval_sequence_export_exp : venv -> pos -> exp list -> export -> venv * value
+val eval : Omake_env.venv -> Omake_ir.exp -> Omake_value_type.value
+val eval_exp : Omake_env.venv -> Omake_value_type.value -> Omake_ir.exp -> Omake_env.venv * Omake_value_type.value
+val eval_sequence : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_ir.exp list -> Omake_env.venv * Omake_value_type.value
+val eval_sequence_export : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_ir.exp list -> Omake_ir.export -> Omake_env.venv * Omake_value_type.value
+val eval_sequence_exp : Omake_env.venv -> Omake_value_type.pos -> Omake_ir.exp list -> Omake_env.venv * Omake_value_type.value
+val eval_sequence_export_exp : Omake_env.venv -> Omake_value_type.pos -> Omake_ir.exp list -> Omake_ir.export -> Omake_env.venv * Omake_value_type.value
 
 (*
  * String expression evaluation.
  *)
-val eval_string_exp : venv -> pos -> string_exp -> value
+val eval_string_exp : Omake_env.venv -> Omake_value_type.pos -> Omake_ir.string_exp -> Omake_value_type.value
 
 (*
  * Include the file literally.
  *)
-val find_include_file : venv -> pos -> loc -> string -> Node.t
-val eval_open_file : venv -> senv_open_file
-val eval_include_file : venv -> include_scope -> pos -> loc -> Node.t -> venv * value
-val include_file : venv -> include_scope -> pos -> loc -> Node.t -> venv
+val find_include_file : Omake_env.venv -> Omake_value_type.pos -> Lm_location.loc -> string -> Omake_node.Node.t
+val eval_open_file : Omake_env.venv -> Omake_ir_ast.senv_open_file
+val eval_include_file : Omake_env.venv -> Omake_env.include_scope -> Omake_value_type.pos -> Lm_location.loc -> Omake_node.Node.t -> Omake_env.venv * Omake_value_type.value
+val include_file : Omake_env.venv -> Omake_env.include_scope -> Omake_value_type.pos -> Lm_location.loc -> Omake_node.Node.t -> Omake_env.venv
 
 (*
  * Evaluate a file as if it were an object.
  *)
-val eval_object_file : venv -> pos -> loc -> Node.t -> obj
+val eval_object_file : Omake_env.venv -> Omake_value_type.pos -> Lm_location.loc -> Omake_node.Node.t -> Omake_value_type.obj
 
 (*
  * Evaluate the program.
  * This modifies the environment.
  *)
-val compile : venv -> unit
+val compile : Omake_env.venv -> unit
 
 (*
  * Passes the IR thru Omake_ir_semant.build_prog, printing it if print_ir is enabled.
  *)
-val postprocess_ir : venv -> ir -> ir
+val postprocess_ir : Omake_env.venv -> Omake_ir.ir -> Omake_ir.ir
 
 (*
  * Evaluate a dependency file.
  *)
-val compile_deps : venv -> Node.t -> string -> (string list * string list) list
+val compile_deps : Omake_env.venv -> Omake_node.Node.t -> string -> (string list * string list) list
 
 (*
  * Remove outermost applications.
  *)
-val eval_value : venv -> pos -> value -> value
+val eval_value : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value
 
 (*
  * Evaluate ValBody expressions.
  *)
-val eval_body_value : venv -> pos -> value -> value
-val eval_body_exp   : venv -> pos -> value -> value -> venv * value
+val eval_body_value : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value
+val eval_body_exp   : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value -> Omake_env.venv * Omake_value_type.value
 
 (*
- * Get the object for the value.
+ * Get the object for the Omake_value_type.value.
  *)
-val eval_object : venv -> pos -> value -> obj
-val eval_find_field : venv -> pos -> loc -> var_info -> var list -> path * obj * var
-val eval_find_method : venv -> pos -> loc -> var_info -> var list -> venv * value
-val eval_defined_field : venv -> pos -> loc -> var_info -> var list -> bool
+val eval_object : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.obj
+val eval_find_field : Omake_env.venv -> Omake_value_type.pos -> Lm_location.loc -> Omake_ir.var_info -> Omake_ir.var list -> Omake_value_type.path * Omake_value_type.obj * Omake_ir.var
+val eval_find_method : Omake_env.venv -> Omake_value_type.pos -> Lm_location.loc -> Omake_ir.var_info -> Omake_ir.var list -> Omake_env.venv * Omake_value_type.value
+val eval_defined_field : Omake_env.venv -> Omake_value_type.pos -> Lm_location.loc -> Omake_ir.var_info -> Omake_ir.var list -> bool
 
 (*
- * Evaluate a value that should be a function.
+ * Evaluate a Omake_value_type.value that should be a function.
  * Be careful with this: don't create a ValPrim using
  * this function, since marshaling will fail.
  *)
-val eval_fun : venv -> pos -> value -> bool * prim_fun_data
+val eval_fun : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> bool * Omake_env.prim_fun_data
 
 (*
- * Also, if the value is an array of 1 element,
+ * Also, if the Omake_value_type.value is an array of 1 element,
  * return the element.
  *)
-val eval_single_value : venv -> pos -> value -> value
+val eval_single_value : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value
 
 (*
- * Evaluate to a primitive value.
- * That is, if the value is an object, return the
+ * Evaluate to a primitive Omake_value_type.value.
+ * That is, if the Omake_value_type.value is an object, return the
  * primitive handle associated with the object.
- * If the object has no primitive value, the object
+ * If the object has no primitive Omake_value_type.value, the object
  * itself is returned.
  *)
-val eval_prim_value : venv -> pos -> value -> value
+val eval_prim_value : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value
 
 (*
  * Evaluate a function application.
  *)
-val eval_apply : venv -> pos -> loc -> value -> value list -> keyword_value list -> venv * value
-val eval_partial_apply : venv -> pos -> loc -> value -> value list -> keyword_value list -> venv * value
+val eval_apply : Omake_env.venv -> Omake_value_type.pos -> Lm_location.loc -> Omake_value_type.value -> Omake_value_type.value list -> Omake_value_type.keyword_value list -> Omake_env.venv * Omake_value_type.value
+val eval_partial_apply : Omake_env.venv -> Omake_value_type.pos -> Lm_location.loc -> Omake_value_type.value -> Omake_value_type.value list -> Omake_value_type.keyword_value list -> Omake_env.venv * Omake_value_type.value
 
 (*
  * Conversions.
  * The following two functions should be used with care, since
- * they fail if the value contains an array.
+ * they fail if the Omake_value_type.value contains an array.
  *)
-val string_of_value : venv -> pos -> value -> string
-val string_of_quote : venv -> pos -> char option -> value list -> string
-val file_of_value   : venv -> pos -> value -> Node.t
-val path_of_values  : venv -> pos -> value list -> string -> (bool * Dir.t list) list
+val string_of_value : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> string
+val string_of_quote : Omake_env.venv -> Omake_value_type.pos -> char option -> Omake_value_type.value list -> string
+val file_of_value   : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_node.Node.t
+val path_of_values  : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value list -> string -> (bool * Omake_node.Dir.t list) list
 
 (*
  * These conversions are safe to use anywhere.
  *)
-val tokens_of_value  : venv -> pos -> lexer -> value -> tok list
-val arg_of_values    : venv -> pos -> value list -> arg
-val argv_of_values   : venv -> pos -> value list list -> arg list
-val values_of_value  : venv -> pos -> value -> value list
-val strings_of_value : venv -> pos -> value -> string list
-val bool_of_value    : venv -> pos -> value -> bool
+val tokens_of_value  : Omake_env.venv -> Omake_value_type.pos -> Omake_env.lexer -> Omake_value_type.value -> Omake_env.tok list
+val arg_of_values    : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value list -> Omake_command_type.arg
+val argv_of_values   : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value list list -> Omake_command_type.arg list
+val values_of_value  : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value list
+val strings_of_value : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> string list
+val bool_of_value    : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> bool
 
 (*
  * -*-
--- src/eval/omake_value.mli
+++ src/eval/omake_value.mli
@@ -1,112 +1,61 @@
-(*
- * Utilities on values.
- *
- * ----------------------------------------------------------------
- *
- * @begin[license]
- * Copyright (C) 2003 Jason Hickey, Caltech
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
- * of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- * Additional permission is given to link this library with the
- * with the Objective Caml runtime, and to redistribute the
- * linked executables.  See the file LICENSE.OMake for more details.
- *
- * Author: Jason Hickey
- * @email{jyh@cs.caltech.edu}
- * @end[license]
- *)
-open Lm_glob
-open Lm_location
+(**
+  If there is only one value,
+  do not create the array. *)
+val concat_array : Omake_value_type.value list -> Omake_value_type.value
 
-open Omake_ir
-open Omake_env
-
-open Omake_node
-open Omake_lexer
-open Omake_parser
-open! Omake_value_type
-
-
-(*
- * If there is only one value,
- * do not create the array.
- *)
-val concat_array : value list -> value
 
 (*
  * Concatenate some strings.
  * If there is only 1 value, do not create the array.
  *)
-val concat_strings : string list -> value
+val concat_strings : string list -> Omake_value_type.value
 
 (*
  * Expand a value so that the outermost constructor
  * is not an application.
  *)
-val eval_value        : venv -> pos -> value -> value
-val eval_single_value : venv -> pos -> value -> value
-val eval_prim_value   : venv -> pos -> value -> value
-val eval_object_value : venv -> pos -> obj -> value
+val eval_value        : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value
+val eval_single_value : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value
+val eval_prim_value   : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value
+val eval_object_value : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.obj -> Omake_value_type.value
 
-val add_object_value  : obj -> value -> obj
+val add_object_value  : Omake_value_type.obj -> Omake_value_type.value -> Omake_value_type.obj
 
 (*
  * Convert to a string.
  *)
-val string_of_value  : venv -> pos -> value -> string
-val strings_of_value : venv -> pos -> value -> string list
-val values_of_value  : venv -> pos -> value -> value list
-val vars_of_value    : venv -> pos -> value -> var_info list
+val string_of_value  : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> string
+val strings_of_value : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> string list
+val values_of_value  : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value list
+val vars_of_value    : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_ir.var_info list
 
 (*
  * Coercions.
  *)
-val bool_of_value       : venv -> pos -> value -> bool
-val int_of_value        : venv -> pos -> value -> int
-val float_of_value      : venv -> pos -> value -> float
-val number_of_value     : venv -> pos -> value -> value
-val key_of_value        : venv -> pos -> value -> value
-val map_of_value        : venv -> pos -> value -> map
+val bool_of_value       : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> bool
+val int_of_value        : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> int
+val float_of_value      : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> float
+val number_of_value     : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value
+val key_of_value        : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value
+val map_of_value        : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.map
 
-val dir_of_value        : venv -> pos -> value -> Dir.t
-val file_of_value       : venv -> pos -> value -> Node.t
-val node_value_of_value : venv -> pos -> ?follow_symlinks:bool -> value -> value
-val dir_value_of_value  : venv -> pos -> value -> value
-val filename_of_value   : venv -> pos -> value -> string
+val dir_of_value        : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_node.Dir.t
+val file_of_value       : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_node.Node.t
+val node_value_of_value : Omake_env.venv -> Omake_value_type.pos -> ?follow_symlinks:bool -> Omake_value_type.value -> Omake_value_type.value
+val dir_value_of_value  : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.value
+val filename_of_value   : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> string
 
-val prim_channel_of_value    : venv -> pos -> value -> prim_channel
-val prim_channel_of_var      : venv -> pos -> loc -> var_info -> prim_channel
-val channel_of_var           : venv -> pos -> loc -> var_info -> Lm_channel.t
-val channel_of_value         : venv -> pos -> value -> Lm_channel.t
-val in_channel_of_any_value  : venv -> pos -> value -> prim_channel * bool
-val out_channel_of_any_value : venv -> pos -> value -> prim_channel * bool
+val prim_channel_of_value    : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.prim_channel
+val prim_channel_of_var      : Omake_env.venv -> Omake_value_type.pos -> Lm_location.loc -> Omake_ir.var_info -> Omake_value_type.prim_channel
+val channel_of_var           : Omake_env.venv -> Omake_value_type.pos -> Lm_location.loc -> Omake_ir.var_info -> Lm_channel.t
+val channel_of_value         : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Lm_channel.t
+val in_channel_of_any_value  : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.prim_channel * bool
+val out_channel_of_any_value : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Omake_value_type.prim_channel * bool
 
-val is_glob_value            : glob_options -> value -> bool
-val is_glob_value_list       : glob_options -> value list -> bool
+val is_glob_value            : Lm_glob.glob_options -> Omake_value_type.value -> bool
+val is_glob_value_list       : Lm_glob.glob_options -> Omake_value_type.value list -> bool
 
-val current_lexer            : venv -> pos -> Lexer.t
-val current_parser           : venv -> pos -> Parser.t
-val loc_of_value             : venv -> pos -> value -> loc
+val current_lexer            : Omake_env.venv -> Omake_value_type.pos -> Omake_lexer.Lexer.t
+val current_parser           : Omake_env.venv -> Omake_value_type.pos -> Omake_parser.Parser.t
+val loc_of_value             : Omake_env.venv -> Omake_value_type.pos -> Omake_value_type.value -> Lm_location.loc
 
-(*!
- * @docoff
- *
- * -*-
- * Local Variables:
- * Caml-master: "compile"
- * End:
- * -*-
- *)
