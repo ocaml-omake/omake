b4643565c1530379043a7961870270377a75f8db
Hongbo Zhang <bobzhang1988@gmail.com>
2014-08-25 22:35:19 -0400

    clean

--- src/builtin/omake_builtin_arith.ml
+++ src/builtin/omake_builtin_arith.ml
@@ -9,23 +9,15 @@
 
 
 
-open Omake_ir
-
-open Omake_pos
-
-
-
-
+(* open Omake_ir *)
 open! Omake_value
-
 open Omake_builtin
-
 open Omake_builtin_util
 open Omake_builtin_type
 open Omake_value_type
 
-module Pos = MakePos (struct let name = "Omake_builtin_arith" end)
-open Pos
+include Omake_pos.MakePos (struct let name = "Omake_builtin_arith" end)
+
 
 (*
  * \begin{doc}
@@ -44,24 +36,24 @@ open Pos
  * \end{doc}
  *)
 let int_fun venv pos loc args =
-   let pos = string_pos "int" pos in
-      match args with
-         [arg] ->
-            let values = values_of_value venv pos arg in
-            let values = List.map (fun v -> ValInt (int_of_value venv pos v)) values in
-               concat_array values
-       | _ ->
-            raise (OmakeException (loc_pos loc pos, ArityMismatch (ArityExact 1, List.length args)))
+  let pos = string_pos "int" pos in
+  match args with
+    [arg] ->
+    let values = values_of_value venv pos arg in
+    let values = List.map (fun v -> ValInt (int_of_value venv pos v)) values in
+    concat_array values
+  | _ ->
+    raise (OmakeException (loc_pos loc pos, ArityMismatch (ArityExact 1, List.length args)))
 
 let float_fun venv pos loc args =
-   let pos = string_pos "int" pos in
-      match args with
-         [arg] ->
-            let values = values_of_value venv pos arg in
-            let values = List.map (fun v -> ValFloat (float_of_value venv pos v)) values in
-               concat_array values
-       | _ ->
-            raise (OmakeException (loc_pos loc pos, ArityMismatch (ArityExact 1, List.length args)))
+  let pos = string_pos "int" pos in
+  match args with
+    [arg] ->
+    let values = values_of_value venv pos arg in
+    let values = List.map (fun v -> ValFloat (float_of_value venv pos v)) values in
+    concat_array values
+  | _ ->
+    raise (OmakeException (loc_pos loc pos, ArityMismatch (ArityExact 1, List.length args)))
 
 (*
  * Basic arithmetic.
@@ -106,74 +98,74 @@ let float_fun venv pos loc args =
  * \end{doc}
  *)
 let unary_int op_int venv pos loc args =
-   let pos = string_pos "unary_int" pos in
-      match args with
-         [arg] ->
-            concat_array (List.map (fun _ -> ValInt (op_int (int_of_value venv pos arg))) (values_of_value venv pos arg))
-       | _ ->
-            raise (OmakeException (loc_pos loc pos, ArityMismatch (ArityExact 1, List.length args)))
+  let pos = string_pos "unary_int" pos in
+  match args with
+    [arg] ->
+    concat_array (List.map (fun _ -> ValInt (op_int (int_of_value venv pos arg))) (values_of_value venv pos arg))
+  | _ ->
+    raise (OmakeException (loc_pos loc pos, ArityMismatch (ArityExact 1, List.length args)))
 
 let arith_int id_int op_int venv pos _ args =
-   let pos = string_pos "arith_int" pos in
-   let collect i arg =
-      op_int i (int_of_value venv pos arg)
-   in
-   let args =
-      match args with
-         [arg] ->
-            values_of_value venv pos arg
-       | _ ->
-            args
-   in
-      match args with
-         arg :: args ->
-            ValInt (List.fold_left collect (int_of_value venv pos arg) args)
-       | [] ->
-            ValInt id_int
+  let pos = string_pos "arith_int" pos in
+  let collect i arg =
+    op_int i (int_of_value venv pos arg)
+  in
+  let args =
+    match args with
+      [arg] ->
+      values_of_value venv pos arg
+    | _ ->
+      args
+  in
+  match args with
+    arg :: args ->
+    ValInt (List.fold_left collect (int_of_value venv pos arg) args)
+  | [] ->
+    ValInt id_int
 
 let unary op_int op_float venv pos loc args =
-   let pos = string_pos "unary" pos in
-      match args with
-         [arg] ->
-            concat_array (List.map (fun v ->
-                                match number_of_value venv pos v with
-                                   ValInt i ->
-                                      ValInt (op_int i)
-                                 | ValFloat x ->
-                                      ValFloat (op_float x)
-                                 | _ ->
-                                      raise (OmakeException (loc_pos loc pos, StringError "not a number"))) (**)
-                             (values_of_value venv pos arg))
-       | _ ->
-            raise (OmakeException (loc_pos loc pos, ArityMismatch (ArityExact 1, List.length args)))
+  let pos = string_pos "unary" pos in
+  match args with
+    [arg] ->
+    concat_array (List.map (fun v ->
+          match number_of_value venv pos v with
+            ValInt i ->
+            ValInt (op_int i)
+          | ValFloat x ->
+            ValFloat (op_float x)
+          | _ ->
+            raise (OmakeException (loc_pos loc pos, StringError "not a number"))) (**)
+          (values_of_value venv pos arg))
+  | _ ->
+    raise (OmakeException (loc_pos loc pos, ArityMismatch (ArityExact 1, List.length args)))
 
 let arith id_int op_int op_float venv pos loc args =
-   let pos = string_pos "arith" pos in
-   let collect i arg =
-      match i, number_of_value venv pos arg with
-         ValInt i, ValInt arg ->
-            ValInt (op_int i arg)
-       | ValInt i, ValFloat arg ->
-            ValFloat (op_float (float_of_int i) arg)
-       | ValFloat i, ValInt arg ->
-            ValFloat (op_float i (float_of_int arg))
-       | ValFloat i, ValFloat arg ->
-            ValFloat (op_float i arg)
-       | _ ->
-            raise (OmakeException (loc_pos loc pos, StringError "not a number"))
-   in
-   let args =
-      match args with
-         [arg] ->
-            values_of_value venv pos arg
-       | _ ->
-            args
-   in
-      match args with
-         arg :: args ->
-            List.fold_left collect (number_of_value venv pos arg) args
-       | [] ->
-            ValInt id_int
+  let pos = string_pos "arith" pos in
+  let collect i arg =
+    match i, number_of_value venv pos arg with
+      ValInt i, ValInt arg ->
+      ValInt (op_int i arg)
+    | ValInt i, ValFloat arg ->
+      ValFloat (op_float (float_of_int i) arg)
+    | ValFloat i, ValInt arg ->
+      ValFloat (op_float i (float_of_int arg))
+    | ValFloat i, ValFloat arg ->
+      ValFloat (op_float i arg)
+    | _ ->
+      raise (OmakeException (loc_pos loc pos, StringError "not a number"))
+  in
+  let args =
+    match args with
+      [arg] ->
+      values_of_value venv pos arg
+    | _ ->
+      args
+  in
+  match args with
+    arg :: args ->
+    List.fold_left collect (number_of_value venv pos arg) args
+  | [] ->
+    ValInt id_int
 
 (*
  * Basic arithmetic.
@@ -206,69 +198,64 @@ let arith id_int op_int op_float venv pos loc args =
  * \end{doc}
  *)
 let compare op_int op_float venv pos loc args =
-   let pos = string_pos "arith" pos in
-   let rec collect i args =
-      match args with
-         arg :: args ->
-            let arg = number_of_value venv pos arg in
-            let test =
-               match i, number_of_value venv pos arg with
-                  ValInt i, ValInt arg ->
-                     op_int i arg
-                | ValInt i, ValFloat arg ->
-                     op_float (float_of_int i) arg
-                | ValFloat i, ValInt arg ->
-                     op_float i (float_of_int arg)
-                | ValFloat i, ValFloat arg ->
-                     op_float i arg
-                | _ ->
-                     raise (OmakeException (loc_pos loc pos, StringError "not a number"))
-            in
-               test && collect arg args
-       | [] ->
-            true
-   in
-      match args with
-         arg :: args ->
-            val_of_bool (collect (number_of_value venv pos arg) args)
-       | [] ->
-            val_true
+  let pos = string_pos "arith" pos in
+  let rec collect i args =
+    match args with
+      arg :: args ->
+      let arg = number_of_value venv pos arg in
+      let test =
+        match i, number_of_value venv pos arg with
+          ValInt i, ValInt arg ->
+          op_int i arg
+        | ValInt i, ValFloat arg ->
+          op_float (float_of_int i) arg
+        | ValFloat i, ValInt arg ->
+          op_float i (float_of_int arg)
+        | ValFloat i, ValFloat arg ->
+          op_float i arg
+        | _ ->
+          raise (OmakeException (loc_pos loc pos, StringError "not a number"))
+      in
+      test && collect arg args
+    | [] ->
+      true
+  in
+  match args with
+    arg :: args ->
+    val_of_bool (collect (number_of_value venv pos arg) args)
+  | [] ->
+    val_true
 
 (************************************************************************
  * Tables.
- *)
-let () =
-   let builtin_funs =
-      [true, "int",             int_fun,                     ArityExact 1;
-       true, "float",           float_fun,                   ArityExact 1;
-       true, "neg",             unary (~-) (~-.),            ArityExact 1;
-       true, "add",             arith 0 ( + ) ( +. ),        ArityAny;
-       true, "sub",             arith 0 ( - ) ( -. ),        ArityAny;
-       true, "mul",             arith 1 ( * ) ( *. ),        ArityAny;
-       true, "div",             arith 1 ( / ) ( /. ),        ArityAny;
-       true, "mod",             arith 1 (mod) (mod_float),   ArityAny;
-       true, "min",             arith max_int min min,       ArityAny;
-       true, "max",             arith min_int max max,       ArityAny;
-       true, "mod",             arith 1 (mod) (mod_float),   ArityAny;
-       true, "lnot",            unary_int (lnot),            ArityExact 1;
-       true, "land",            arith_int 0 (land),          ArityAny;
-       true, "lor",             arith_int 0 (lor) ,          ArityAny;
-       true, "lxor",            arith_int 0 (lxor),          ArityAny;
-       true, "lsl",             arith_int 0 (lsl),           ArityAny;
-       true, "lsr",             arith_int 0 (lsr),           ArityAny;
-       true, "asr",             arith_int 0 (asr),           ArityAny;
-       true, "lt",              compare (<) (<),             ArityAny;
-       true, "le",              compare (<=) (<=),           ArityAny;
-       true, "eq",              compare (=) (=),             ArityAny;
-       true, "ge",              compare (>=) (>=),           ArityAny;
-       true, "gt",              compare (>) (>),             ArityAny]
-   in
-   let builtin_info = { builtin_empty with builtin_funs = builtin_funs } in
-      register_builtin builtin_info
+*)
+let builtin_funs =
+  [true, "int",             int_fun,                     Omake_ir.ArityExact 1;
+   true, "float",           float_fun,                   ArityExact 1;
+   true, "neg",             unary (~-) (~-.),            ArityExact 1;
+   true, "add",             arith 0 ( + ) ( +. ),        ArityAny;
+   true, "sub",             arith 0 ( - ) ( -. ),        ArityAny;
+   true, "mul",             arith 1 ( * ) ( *. ),        ArityAny;
+   true, "div",             arith 1 ( / ) ( /. ),        ArityAny;
+   true, "mod",             arith 1 (mod) (mod_float),   ArityAny;
+   true, "min",             arith max_int min min,       ArityAny;
+   true, "max",             arith min_int max max,       ArityAny;
+   true, "mod",             arith 1 (mod) (mod_float),   ArityAny;
+   true, "lnot",            unary_int (lnot),            ArityExact 1;
+   true, "land",            arith_int 0 (land),          ArityAny;
+   true, "lor",             arith_int 0 (lor) ,          ArityAny;
+   true, "lxor",            arith_int 0 (lxor),          ArityAny;
+   true, "lsl",             arith_int 0 (lsl),           ArityAny;
+   true, "lsr",             arith_int 0 (lsr),           ArityAny;
+   true, "asr",             arith_int 0 (asr),           ArityAny;
+   true, "lt",              compare (<) (<),             ArityAny;
+   true, "le",              compare (<=) (<=),           ArityAny;
+   true, "eq",              compare (=) (=),             ArityAny;
+   true, "ge",              compare (>=) (>=),           ArityAny;
+   true, "gt",              compare (>) (>),             ArityAny]
 
-(*
- * -*-
- * Local Variables:
- * End:
- * -*-
- *)
+
+let () = 
+  let builtin_info = { builtin_empty with builtin_funs = builtin_funs } in
+  register_builtin builtin_info
+
