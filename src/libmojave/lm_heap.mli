879990d2e2d02bdcdacb4fa7f0921d1a82112691
Hongbo Zhang <bobzhang1988@gmail.com>
2014-08-31 22:01:51 -0400

    remove the symlink to make bootstrap easier

---
+++ src/libmojave/lm_heap.mli
@@ -0,0 +1,42 @@
+(*
+ * For heap debugging.
+ *
+ * ----------------------------------------------------------------
+ *
+ * @begin[license]
+ * Copyright (C) 2006 Mojave Group, Caltech
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation,
+ * version 2.1 of the License.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Additional permission is given to link this library with the
+ * OpenSSL project's "OpenSSL" library, and with the OCaml runtime,
+ * and you may distribute the linked executables.  See the file
+ * LICENSE.libmojave for more details.
+ *
+ * Author: Jason Hickey
+ * @email{jyh@cs.caltech.edu}
+ * @end[license]
+ *)
+
+val heap_check : string -> unit
+
+(*
+ * -*-
+ * Local Variables:
+ * Fill-column: 100
+ * End:
+ * -*-
+ * vim:ts=3:et:tw=100
+ *)
