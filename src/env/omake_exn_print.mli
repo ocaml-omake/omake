acc7595221f496e71c11bcde54839e49a7d95f3f
Gerd Stolpmann <gerd@edgespring.com>
2014-10-17 21:10:30 +0200

    Enabling backtraces

--- src/env/omake_exn_print.mli
+++ src/env/omake_exn_print.mli
@@ -9,6 +9,8 @@ val is_shell_exn : exn -> bool
  * Print an exception.
  *)
 val pp_print_exn : exn Lm_printf.t 
+val pp_print_exn_with_backtrace : backtrace:string -> exn Lm_printf.t 
+
 
 (*
  * Generic catcher.
